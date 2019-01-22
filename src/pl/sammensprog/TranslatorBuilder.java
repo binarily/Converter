24452d41b463e1c4e15d28ae3a60304fdd58b6f3
Kamil Czerniak <kamil_czerniak@outlook.com>
2019-01-22 17:01:11 +0100

    Making TranslatorBuilder public

--- src/pl/sammensprog/TranslatorBuilder.java
+++ src/pl/sammensprog/TranslatorBuilder.java
@@ -1,8 +1,7 @@
 package pl.sammensprog;
 
-import java.util.Comparator;
 import java.util.HashMap;
 
-abstract class TranslatorBuilder {
+public abstract class TranslatorBuilder {
     abstract Translator build(HashMap<String, String> settings, HashMap<String, Assertion[]> definedObjectSettings);
 }
